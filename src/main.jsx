58da09021d473d0b91cdd0a1fb3d19024864c70e
jhjhc1483 <jfchae1483@naver.com>
2025-12-10 20:26:52 +0900

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+<p> 안녕하세요.</p>
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+)
